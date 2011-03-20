1a658a202e421fc3be5dff421f31aff7dd9cc0d8
Yasuharu NAKANO <ynak@jggug.org>
2011-03-20 11:54:16 +0900

    homebrew's formula

---
+++ git-now.rb
@@ -0,0 +1,10 @@
+require 'formula'
+
+class GitNow < GithubGistFormula
+  url 'https://gist.github.com/raw/705408/69e8cea56420b74b2e771e883b6291dfe2094d0d/git-now'
+  md5 '0f442e6c545b8e8fee37217c176b1940'
+
+  def install
+    bin.install 'git-now'
+  end
+end
