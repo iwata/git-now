3d07c6f49302d78db3153fa0285781bf5340563f
Yasuharu NAKANO <ynak@jggug.org>
2011-03-20 12:46:38 +0900

    fixed formula

--- git-now.rb
+++ git-now.rb
@@ -3,8 +3,5 @@ require 'formula'
 class GitNow < GithubGistFormula
   url 'https://gist.github.com/raw/705408/69e8cea56420b74b2e771e883b6291dfe2094d0d/git-now'
   md5 '0f442e6c545b8e8fee37217c176b1940'
-
-  def install
-    bin.install 'git-now'
-  end
+  homepage 'https://gist.github.com/878015/'
 end
