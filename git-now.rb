04c31837fc3f13355daf17e639730616df11316e
Yasuharu NAKANO <ynak@jggug.org>
2011-07-26 01:29:34 +0900

    update formula for a0d5d83085ba202e60234c4920182cebd06aaf00

--- git-now.rb
+++ git-now.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class GitNow < GithubGistFormula
-  url 'https://gist.github.com/raw/705408/69e8cea56420b74b2e771e883b6291dfe2094d0d/git-now'
-  md5 '0f442e6c545b8e8fee37217c176b1940'
+  url 'https://raw.github.com/gist/878015/8a7158b2bfce40415b3a0394afd45c442addfe06/git-now'
+  md5 '144c3759343e59187dffd8a7c4cf61a2'
   homepage 'https://gist.github.com/878015/'
 end
