b312880c3ef6a199422abaf02fc6f051956e231f
Motonori Iwata <iwata.motonori@dena.jp>
2011-09-03 23:54:33 +0900

    update Formula version

--- git-now.rb
+++ git-now.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class GitNow < Formula
-  url  'https://github.com/iwata/git-now.git', :tag => 'v0.0.2'
-  version '0.0.2'
+  url  'https://github.com/iwata/git-now.git', :tag => 'v0.0.22'
+  version '0.0.22'
   head 'https://github.com/iwata/git-now.git', :branch => 'develop'
   homepage 'https://github.com/iwata/git-now'
 
