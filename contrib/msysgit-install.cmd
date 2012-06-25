cc1b6360da43ec87f99ebedafec51ce4771f01f8
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2012-06-25 13:04:10 +0900

    Create install script for msysgit

---
+++ contrib/msysgit-install.cmd
@@ -0,0 +1,23 @@
+set STARTUP_PATH=%~d0%~p0
+if "%HOME%" == "" goto sethome
+goto makedir
+
+:sethome
+echo set HOME variable
+set HOME=%UserProfile%
+
+:makedir
+if exist %HOME%\bin goto :copyshflags
+echo make bin folder
+mkdir %HOME%\bin
+
+set MSYS_BIN=%HOME%\bin
+
+echo Installing git-now to "%MSYS_BIN%"
+
+:copyshflags
+echo copy shFlags
+copy %STARTUP_PATH%..\shFlags\src\shflags %MSYS_BIN%\gitnow-shFlags
+
+:copygitnow
+xcopy /EXCLUDE:%STARTUP_PATH%exclude.txt /D %STARTUP_PATH%..\* %MSYS_BIN%
